8fa5cddc4ae636e2b9fe9b444fbb79ac0a58893c
pc <pc@PC-BILGISAYAR>
2021-03-05 17:06:38 +0300

    kamp

---
+++ OOP2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Yağız";
+            musteri1.Soyadi = "Kadiroğlu";
+            musteri1.TcNo = "45817130762";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+        }
+    }
+}
